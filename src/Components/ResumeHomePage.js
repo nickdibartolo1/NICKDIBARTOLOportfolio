472ccdf37acbad07affa2907318898c21d2e4ac1
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2023-06-06 19:06:52 -0400

    hopefully the last push, fixed margin on home resume button

--- src/Components/ResumeHomePage.js
+++ src/Components/ResumeHomePage.js
@@ -11,10 +11,13 @@ const ResumeHomePage = () => {
 
             <main className='flex flex-col items-center w-full bg-gradient-to-r from-[#5095f0] to-[#164888]'>
                 <a href={PDF}>
-                    <button className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 flex justify-center items-center p-3 rounded-full cursor-pointer outline-none mt-14'>
+                    <button
+                        className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 flex justify-center items-center p-3 rounded-full cursor-pointer outline-none mt-6 mb-16'
+                    >
                         Get Resume!
                     </button>
                 </a>
+
             </main>
         </div>
     )
